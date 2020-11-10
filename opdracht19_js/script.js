d21ef0076dc778a8a7ff5b8f6df5b20660297f18
vancapelledavid <71630143+vancapelledavid@users.noreply.github.com>
2020-11-10 14:03:19 +0100

    Add files via upload

---
+++ opdracht19_js/script.js
@@ -0,0 +1,11 @@
+console.log("Hello Winc Academy")
+let name= 'David';
+console.log(name);
+console.log(4+4);
+console.log("4"+"4")
+console.log(5+4-8*3);
+console.log((7+8)/2)
+console.log(100%3)
+let age="48";
+console.log(age);
+console.log (typeof(age));
